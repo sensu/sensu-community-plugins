27676407ea4b5cc237c05089ea7ce0f87b105e84
Sean Porter <portertech@gmail.com>
2011-11-03 20:00:40 -0700

    apache license plugins

--- plugins/puppet/puppet_agent.rb
+++ plugins/puppet/puppet_agent.rb
@@ -13,21 +13,26 @@
 # limitations under the License.
 #
 #!/usr/bin/env ruby
- `which tasklist`
- case
- when $? == 0
-   procs = `tasklist`
- else
-   procs = `ps aux`
- end
- running = false
- procs.each_line do |proc|
-   running = true if proc.grep(/puppetd|puppet agent/)
- end
- if running
-   puts 'PUPPET AGENT - OK - Puppet agent is running'
-   exit 0
- else
-   puts 'PUPPET AGENT - WARNING - Puppet agent is NOT running'
-   exit 1
- end
+
+#
+# This plugin checks to see if the Puppet Labs Puppet agent is running
+#
+
+`which tasklist`
+case
+when $? == 0
+  procs = `tasklist`
+else
+  procs = `ps aux`
+end
+running = false
+procs.each_line do |proc|
+  running = true if proc.grep(/puppetd|puppet agent/)
+end
+if running
+  puts 'PUPPET AGENT - OK - Puppet agent is running'
+  exit 0
+else
+  puts 'PUPPET AGENT - WARNING - Puppet agent is NOT running'
+  exit 1
+end
