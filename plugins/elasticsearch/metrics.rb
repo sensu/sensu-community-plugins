d22edd348a2ec81c321b84f19ddf6417a5b029df
Sean Porter <portertech@gmail.com>
2011-11-06 17:39:36 -0800

    fix license on a few plugins

--- plugins/elasticsearch/metrics.rb
+++ plugins/elasticsearch/metrics.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+#
 # Copyright 2011 Sonian Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -10,7 +11,7 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-#implied.
+# implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
--- plugins/puppet/puppet_agent.rb
+++ plugins/puppet/puppet_agent.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+#
 # Copyright 2011 James Turnbull
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
