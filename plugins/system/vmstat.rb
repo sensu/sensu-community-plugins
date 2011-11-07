c4f8061a46b2cf6f86cbe1e58e1013777f873cfd
Sean Porter <portertech@gmail.com>
2011-11-07 08:34:35 -0800

    [system] correct vmstat plugin name

--- plugins/system/vmstat.rb
+++ plugins/system/vmstat.rb
@@ -10,12 +10,11 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Chef Client Plugin
+# System VMStat Plugin
 # ===
 #
 # This plugin uses vmstat to collect basic system metrics, produces
