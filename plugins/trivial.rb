7497fbefe1812b5ca0b1f26daf62b52a879d4922
Decklin Foster <decklin@red-bean.com>
2011-12-13 10:43:55 -0500

    Add an example option (to generate a critical) to the trivial plugin

--- plugins/trivial.rb
+++ plugins/trivial.rb
@@ -4,8 +4,18 @@ require 'sensu-plugin/check/cli'
 
 class Trivial < Sensu::Plugin::Check::CLI
 
+  option :fail,
+         :description => "Simulate failure",
+         :boolean => true,
+         :short => '-f',
+         :long => '--fail'
+
   def run
-    ok "All is well"
+    if config[:fail]
+      critical "Something went wrong"
+    else
+      ok "All is well"
+    end
   end
 
 end
