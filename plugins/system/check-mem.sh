66d27720d9147a4e2a8e04bed2b9ea34776e7f31
Norman J. Harman Jr <njharman@gmail.com>
2012-04-02 12:41:43 -0500

    Simplified script after it didn't run correctly for me. Also, I believe the original scrit checked warnings first and thus never reported critical events.

--- plugins/system/check-mem.sh
+++ plugins/system/check-mem.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #
-# evaluate free system memory from Linux based systems
+# Evaluate free system memory from Linux based systems.
 #
 # Date: 2007-11-12
 # Author: Thomas Borger - ESG
 #
-# the memory check is done with following command line:
+# The memory check is done with following command line:
 # free -m | grep buffers/cache | awk '{ print $4 }'
 
 # get arguments
 
 while getopts 'w:c:hp' OPT; do
   case $OPT in
-    w)  int_warn=$OPTARG;;
-    c)  int_crit=$OPTARG;;
+    w)  WARN=$OPTARG;;
+    c)  CRIT=$OPTARG;;
     h)  hlp="yes";;
     p)  perform="yes";;
     *)  unknown="yes";;
@@ -24,12 +24,10 @@ done
 HELP="
     usage: $0 [ -w value -c value -p -h ]
 
-    syntax:
-
-            -w --> Warning integer value
-            -c --> Critical integer value
-            -p --> print out performance data
-            -h --> print this help screen
+        -w --> Warning MB < value
+        -c --> Critical MB < value
+        -p --> print out performance data
+        -h --> print this help screen
 "
 
 if [ "$hlp" = "yes" -o $# -lt 1 ]; then
@@ -37,61 +35,29 @@ if [ "$hlp" = "yes" -o $# -lt 1 ]; then
   exit 0
 fi
 
-# get free memory
-FMEM=`free -m | grep buffers/cache | awk '{ print $4 }'`
+WARN=${WARN:=0}
+CRIT=${CRIT:=0}
 
-# output with or without performance data
-if [ "$perform" = "yes" ]; then
-  OUTPUTP="free system memory: $FMEM MB | free memory="$FMEM"MB;$int_warn;$int_crit;0"
-else
-  OUTPUT="free system memory: $FMEM MB"
-fi
+FREE_MEMORY=`free -m | grep buffers/cache | awk '{ print $4 }'`
 
-if [ -n "$int_warn" -a -n "$int_crit" ]; then
-
-  err=0
-
-  if (( $FMEM <= $int_warn )); then
-    err=1
-  elif (( $FMEM <= $int_crit )); then
-    err=2
-  fi
-
-  if (( $err == 0 )); then
-
-    if [ "$perform" = "yes" ]; then
-      echo "MEM OK - $OUTPUTP"
-      exit "$err"
-    else
-      echo "MEM OK - $OUTPUT"
-      exit "$err"
-    fi
-
-  elif (( $err == 1 )); then
-    if [ "$perform" = "yes" ]; then
-      echo "MEM WARNING - $OUTPUTP"
-      exit "$err"
-    else
-      echo "MEM WARNING - $OUTOUT"
-      exit "$err"
-    fi
-
-  elif (( $err == 2 )); then
-
-    if [ "$perform" = "yes" ]; then
-      echo "MEM CRITICAL - $OUTPUTP"
-      exit "$err"
-    else
-      echo "MEM CRITICAL - $OUTPUT"
-      exit "$err"
-    fi
-
-  fi
-
-else
-
+if [ "$FREE_MEMORY" = "" ]; then
   echo "no output from plugin"
   exit 3
-
 fi
-exit
+
+if [ "$perform" = "yes" ]; then
+  output="free system memory: $FREE_MEMORY MB | free memory="$FREE_MEMORY"MB;$WARN;$CRIT;0"
+else
+  output="free system memory: $FREE_MEMORY MB"
+fi
+
+if (( $FREE_MEMORY <= $CRIT )); then
+  echo "MEM CRITICAL - $output"
+  exit 2
+elif (( $FREE_MEMORY <= $WARN )); then
+  echo "MEM WARNING - $output"
+  exit 1
+else
+  echo "MEM OK - $output"
+  exit 0
+fi
