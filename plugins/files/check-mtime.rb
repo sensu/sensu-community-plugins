450a584dfcd8033d0b6b6b93ad90802b1048b886
Sean Porter <portertech@gmail.com>
2012-12-18 20:46:57 +0000

    cleaned up check mtime and tail

--- plugins/files/check-mtime.rb
+++ plugins/files/check-mtime.rb
@@ -1,21 +1,35 @@
 #!/usr/bin/env ruby
 #
-# Check File mtime Plugin
+# Checks a file's mtime
 # ===
 #
-# This plugin checks a given file's mtime
+# DESCRIPTION:
+#   This plugin checks a given file's modified time.
+#
+# OUTPUT:
+#   plain-text
+#
+# PLATFORMS:
+#   linux
+#   bsd
+#
+# DEPENDENCIES:
+#   sensu-plugin Ruby gem
+#
+# Released under the same terms as Sensu (the MIT license); see LICENSE
+# for details.
 
 require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'sensu-plugin/check/cli'
 require 'fileutils'
 
-class CheckMtime < Sensu::Plugin::Check::CLI
+class Mtime < Sensu::Plugin::Check::CLI
 
   option :file,
     :description => 'File to check last modified time',
     :short => '-f FILE',
     :long => '--file FILE'
-  
+
   option :warn_age,
     :description => 'Warn if mtime greater than provided age in seconds',
     :short => '-w SECONDS',
@@ -26,21 +40,21 @@ class CheckMtime < Sensu::Plugin::Check::CLI
     :short => '-c SECONDS',
     :long => '--critical SECONDS'
 
-  def run
-    unknown 'No file specified' unless config[:file]
-    unknown 'No warn or critical age specified' unless config[:warn_age] || config[:critical_age]
-    if(File.exists?(config[:file]))
-      age = Time.now.to_i - File.mtime(config[:file]).to_i 
-      run_check(:critical, age) || run_check(:warning, age) || ok("file is #{age} seconds old")
-    else
-      critical 'file not found'
-    end
-  end
-
   def run_check(type, age)
     to_check = config["#{type}_age".to_sym].to_i
     if(to_check > 0 && age >= to_check)
       send(type, "file is #{age - to_check} seconds past #{type}")
+    end
+  end
+
+  def run
+    unknown 'No file specified' unless config[:file]
+    unknown 'No warn or critical age specified' unless config[:warn_age] || config[:critical_age]
+    if(File.exists?(config[:file]))
+      age = Time.now.to_i - File.mtime(config[:file]).to_i
+      run_check(:critical, age) || run_check(:warning, age) || ok("file is #{age} seconds old")
+    else
+      critical 'file not found'
     end
   end
 
