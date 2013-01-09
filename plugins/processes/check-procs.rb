94d72c22afff68b72e45b4fe1a1ab964141625c0
Bryan Brandau <bryan.brandau@bestbuy.com>
2013-01-09 16:13:13 -0600

    Revert "add another note."
    This reverts commit 8a26cb7badf1b74efe1b9e414a7408ca55214a10.

--- plugins/processes/check-procs.rb
+++ plugins/processes/check-procs.rb
@@ -11,9 +11,6 @@
 #
 # Requires WMI for Windows support.
 #
-# This should work better than shelling to 'ps', since Proctable should
-# abstract platform differences, but it's untested
-#
 # Examples:
 #
 #   # chef-client is running
