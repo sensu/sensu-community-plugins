e3908faa4cf23a34a016770407e76b7b7b3d5ff4
kcrayon <github.com@crayon.at>
2013-03-15 13:55:41 -0700

    redis_output handler extension

---
+++ extensions/handlers/redis_output.rb
@@ -0,0 +1,35 @@
+module Sensu
+  module Extension
+    class RedisOutput < Handler
+      def name
+        'redis_output'
+      end
+
+      def description
+        'outputs events output to a redis list or channel'
+      end
+
+      def run(event, settings, &block)
+        opts = settings["redis_output"]
+
+        opts["db"]   ||= 0
+        opts["port"] ||= 6379
+        @@redis ||= Redis.connect(:host => opts["host"], :port => opts["port"], :db => opts["db"])
+
+        output = JSON.parse(event)["check"]["output"]
+        output = opts["split"] ? output.split("\n") : Array(output)
+
+        output.each do |e|
+          case opts["data_type"]
+          when "list"
+            @@redis.lpush(opts["key"], e)
+          when "channel"
+            @@redis.publish(opts["key"], e)
+          end
+        end
+
+        block.call("sent #{output.count} events", 0)
+      end
+    end
+  end
+end
