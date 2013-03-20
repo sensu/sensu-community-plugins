3e9b87339cb360a42289f6f65aaf16cc83959850
Jessica Blackburn <jessica.blackburn@opower.com>
2013-03-20 15:40:57 -0400

    Wrap data elements to prevent abnormal exit

--- plugins/rabbitmq/rabbitmq-overview-metrics.rb
+++ plugins/rabbitmq/rabbitmq-overview-metrics.rb
@@ -99,20 +99,22 @@ class RabbitMQMetrics < Sensu::Plugin::Metric::CLI::Graphite
     output "#{config[:scheme]}.queue_totals.messages_ready.rate", overview['queue_totals']['messages_ready_details']['rate'], timestamp
 
     # overview['message_stats']['publish']
-    if overview['message_stats']['publish'] then 
+    if overview['message_stats'].include?('publish') then 
       output "#{config[:scheme]}.message_stats.publish.count", overview['message_stats']['publish'], timestamp
       output "#{config[:scheme]}.message_stats.publish.rate", overview['message_stats']['publish_details']['rate'], timestamp 
     end
 
     # overview['message_stats']['deliver_no_ack']
-    if overview['message_stats']['deliver_no_ack_details'] then 
+    if overview['message_stats'].include?('deliver_no_ack_details') then 
       output "#{config[:scheme]}.message_stats.deliver_no_ack.count", overview['message_stats']['deliver_no_ack'], timestamp
       output "#{config[:scheme]}.message_stats.deliver_no_ack.rate", overview['message_stats']['deliver_no_ack_details']['rate'], timestamp
     end
 
     # overview['message_stats']['deliver_get']
-    output "#{config[:scheme]}.message_stats.deliver_get.count", overview['message_stats']['deliver_get'], timestamp
-    output "#{config[:scheme]}.message_stats.deliver_get.rate", overview['message_stats']['deliver_get_details']['rate'], timestamp
+    if overview['message_stats'].include?('deliver_get') then
+      output "#{config[:scheme]}.message_stats.deliver_get.count", overview['message_stats']['deliver_get'], timestamp
+      output "#{config[:scheme]}.message_stats.deliver_get.rate", overview['message_stats']['deliver_get_details']['rate'], timestamp
+    end 
 
     ok
   end
