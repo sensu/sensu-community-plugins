a12efa8c9322016f164d280fe45175cf7693cfdf
deepakmdass88 <deepak@deeproot.in>
2013-04-24 22:10:15 +0530

    Added two plugins, check-snmp and check-ping

---
+++ plugins/ping/check-ping.rb
@@ -0,0 +1,54 @@
+# Check-ping
+# ===
+#
+# This is a simple Ping check script for Sensu, Currently works with
+#  ICMP as well as HTTP ping.
+#
+# Requires "net-ping" gem
+#
+# Examples:
+#
+#   check-ping -h host -t type -p port    => port option is for HTTP ping
+#   
+#  Deaful host is "google.com", change to if you dont want to pass host  
+# option 
+#  Author Deepak Mohan Dass   <deepakmdass88@gmail.com>
+#
+# Released under the same terms as Sensu (the MIT license); see LICENSE
+# for details.
+
+require 'rubygems' if RUBY_VERSION < '1.9.0'
+require 'sensu-plugin/check/cli'
+require 'net/ping'
+
+class CheckProcs < Sensu::Plugin::Check::CLI
+
+
+option :port, :short => '-p port', :default => "80"
+option :host, :short => '-h host', :default => "google.com"
+option :type, :short => '-t type', :default => 'HTTP'
+
+def run
+	if "#{config[:type]}" == "HTTP"
+		port_num = eval "#{config[:port]}"
+	    pt = Net::Ping::HTTP.new("http://#{config[:host]}", port="#{port_num}", timeout=10)
+		if pt.ping?
+  		   msg = "HTTP ping successful"
+		ok msg
+		else
+  		   msg = "HTTP ping unsuccessful"
+		 critical msg 
+		end
+
+	else
+	    pn = Net::Ping::ICMP.new("#{config[:host]}")
+		if pn.ping?
+  		   msg = "ICMP ping successful"
+		 ok msg
+		else
+  		   msg = "ICMP ping unsuccessful"
+		critical msg
+		end
+	end
+  end
+end
---
+++ plugins/snmp/check-snmp.rb
@@ -0,0 +1,64 @@
+#!/usr/bin/env ruby
+# Check SNMP
+# ===
+#
+# This is a simple SNMP check script for Sensu, We need to supply details like
+# Server, port, SNMP community, and Limits
+#
+#
+# Requires SNMP gem
+#
+# Examples:
+#
+#   check-snmp -h host -C community -O oid -w warning -c critical
+#
+#
+#  Author Deepak Mohan Das   <deepakmdass88@gmail.com>
+#
+# Released under the same terms as Sensu (the MIT license); see LICENSE
+# for details.
+
+
+require 'rubygems' if RUBY_VERSION < '1.9.0'
+require 'sensu-plugin/check/cli'
+require 'snmp'
+
+
+class CheckProcs < Sensu::Plugin::Check::CLI
+
+  def self.read_pid(path)
+    begin
+      File.read(path).chomp.to_i
+    rescue
+      self.new.unknown "Could not read pid file #{path}"
+    end
+  end
+
+option :host, :short => '-h host', :boolean => true, :default => "127.0.0.1"
+option :community, :short => '-C snmp community', :boolean =>true, :default => "public"
+option :objectid, :short => '-O OID', :default => "1.3.6.1.4.1.2021.10.1.3.1"
+option :warning, :short => '-w warning', :default => "1"
+option :critical, :short => '-c critical', :default => "2"
+
+def run
+manager = SNMP::Manager.new(:host => "#{config[:host]}", :community => "#{config[:community]}" )
+response = manager.get(["#{config[:objectid]}"])
+ response.each_varbind do |vb|
+
+	 if "#{vb.value.to_s}".to_i >= "#{config[:critical]}".to_i
+    		msg = "Critical state detected"
+  	  	critical msg
+    	 end
+      	   if (("#{vb.value.to_s}".to_i >= "#{config[:warning]}".to_i) && ("#{vb.value.to_s}".to_i < "#{config[:critical]}".to_i)) 
+      	  	 msg = "Warning state detected"
+     	         warning msg
+            end
+	  if ("#{vb.value.to_s}".to_i < "#{config[:warning]}".to_i)
+        	 msg = "All is well Dude"
+	    	 ok msg
+   	    end
+	  
+     end
+  manager.close
+  end
+end
