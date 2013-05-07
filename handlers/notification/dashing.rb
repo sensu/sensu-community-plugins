1f6b1446ea933113e0ad6c58e230c20cbf2c3b3c
Bowen Masco <bowen@codingfoo.com>
2013-05-07 14:27:10 -0600

    Add license info

--- handlers/notification/dashing.rb
+++ handlers/notification/dashing.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/env ruby
+#
+# Released under the same terms as Sensu (the MIT license); see LICENSE
+# for details
 
 require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'sensu-handler'
