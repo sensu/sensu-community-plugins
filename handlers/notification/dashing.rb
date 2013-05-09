c1510eddf30f005df459bad3c1a4ec6b06c5399b
Bowen Masco <bowen@codingfoo.com>
2013-05-08 19:37:52 -0600

    add documentation

--- handlers/notification/dashing.rb
+++ handlers/notification/dashing.rb
@@ -1,5 +1,23 @@
 #!/usr/bin/env ruby
 #
+# Sensu Dashing(http://shopify.github.io/dashing/) dashboard notifier
+#
+# This handler sends event information to the dashing api.
+#
+# The handler pushes event output to widgets named following a convention of:
+#  clientname-checkname
+#
+# For example, if you have a client named mysqlserver and a check named check_diskusage,
+# it will send events to a widget with the data-id = mysqlserver-check_diskusage
+#
+# The event output will be pushed to the moreinfo data-bind of a widget.
+#
+# This works with a text dashing widget and any others that have a moreinfo data-binding.
+#
+# Two settings are required in dashing.json
+#   auth_token  :  The shared token from you dashing instance.
+#   host        :  The ip and port of the dashing instance.
+#
 # Released under the same terms as Sensu (the MIT license); see LICENSE
 # for details
 
