e0db94f0084ecb370a6541cc1bfb270c420f079e
kepes.peter <kepes.peter@codeplay.hu>
2013-09-05 16:16:13 +0200

    Create a parameter file for config parameters
    - Ruby 1.9 tested
    - XMPP and Google Hangouts problem solved (see notes)

--- handlers/notification/gtalk.rb
+++ handlers/notification/gtalk.rb
@@ -6,14 +6,20 @@
 # This is a simple Gtalk Handler script for Sensu, Add the GMil credentials and
 # Recipient's Gmail id. Currently the message contains Event and Host
 #
-# Note:- Not compatible with Ruby Version > 1.9
+# Notes:
+#   - for Ruby Version > 1.9 compatibility you need to install "xmpp4r-simple-19" gem 
+#   - if sender is registered for Google Hangouts he can't send message. Maybe it is a
+#     problem with XMPP and Hangouts. In this case you need to register a new gmail
+#     account and don't register it for Hangouts
+#   - If client is registered for Hangouts he will not get the message. Client has to 
+#     change IM settings to Old version in Gmail page (left side)
 #
 #  Author Deepak Mohan Das   <deepakmdass88@gmail.com>
 #
 # Released under the same terms as Sensu (the MIT license); see LICENSE
 # for details.
 
-require 'rubygems'
+require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'sensu-handler'
 require 'xmpp4r-simple'
 require 'timeout'
@@ -30,9 +36,6 @@ class GTALK < Sensu::Handler
   end
 
   def handle
-    username = 'testmaster'
-    password = 'secreto'
-    to_username = 'deepakmdass88'
     body = <<-BODY.gsub(/^ {14}/, '')
             #{@event['check']['output']}
             Host: #{@event['client']['name']}
@@ -40,15 +43,18 @@ class GTALK < Sensu::Handler
 
     begin
       timeout 10 do
-        puts "Connecting to jabber server.."
-        jabber = Jabber::Simple.new(username+'@gmail.com', password)
-        puts "Connected."
-        jabber.deliver(to_username+"@gmail.com", "#{body}")
-        sleep(10)
-        puts "Alert successfully sent to #{to_username}"
+        puts "gtalk -- Connecting to jabber server with user #{settings["gtalk"]["mail"]}"
+        jabber = Jabber::Simple.new(settings["gtalk"]["mail"], settings["gtalk"]["password"])
+        puts "gtalk -- Connected"
+        settings["gtalk"]["recipients"].each do |rcp|
+          puts "gtalk -- Sending alert to #{rcp}"
+          jabber.deliver(rcp, "#{body}")
+        end
+        sleep(5)
+        puts "gtalk -- Alert successfully sent to recipients"
       end
     rescue Timeout::Error
-      puts "timed out while attempting to sent message"
+      puts "gtalk -- timed out while attempting to sent message"
     end
   end
 end
