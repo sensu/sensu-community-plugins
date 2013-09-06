3996dd7da48685d582066d3dbf4b66360d9a843f
Brian Clark <brian.clark@cloudapt.com>
2013-09-06 14:55:16 +0000

    Add ignoreline and includeline args to check-disk.rb The -l option will ignore df lines matching comma-seperated list of regex pattern(s). The -L option will only include df lines matching pattern(s). Also added --debug (-d) option to output matched lines for testing purposes.

--- plugins/system/check-disk.rb
+++ plugins/system/check-disk.rb
@@ -14,7 +14,6 @@
 require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'sensu-plugin/check/cli'
 
-# Sensu check class for checking disks
 class CheckDisk < Sensu::Plugin::Check::CLI
 
   option :fstype,
@@ -30,8 +29,14 @@ class CheckDisk < Sensu::Plugin::Check::CLI
     :proc => proc {|a| a.split(',') }
 
   option :ignoreline,
-    :short => '-l TEXT',
-    :proc => proc {|a| a.to_s }
+    :short => '-l PATTERN[,PATTERN]',
+    :description => 'Ignore df line(s) matching pattern(s)',
+    :proc => proc { |a| a.split(',') }
+
+  option :includeline,
+    :short => '-L PATTERN[,PATTERN]',
+    :description => 'Only include df line(s) matching pattern(s)',
+    :proc => proc { |a| a.split(',') }
 
   option :warn,
     :short => '-w PERCENT',
@@ -43,23 +48,32 @@ class CheckDisk < Sensu::Plugin::Check::CLI
     :proc => proc {|a| a.to_i },
     :default => 95
 
+  option :debug,
+      :short => '-d',
+      :long => '--debug',
+      :description => 'Output list of included filesystems'
+
   def initialize
     super
     @crit_fs = []
     @warn_fs = []
+    @line_count = 0
   end
 
   def read_df
     `df -PT`.split("\n").drop(1).each do |line|
       begin
         _fs, type, _blocks, _used, _avail, capacity, mnt = line.split
+        next if config[:includeline] && !config[:includeline].find { |x| line.match(x) }
         next if config[:fstype] && !config[:fstype].include?(type)
         next if config[:ignoretype] && config[:ignoretype].include?(type)
         next if config[:ignoremnt] && config[:ignoremnt].include?(mnt)
-        next if config[:ignoreline] && line.include?(config[:ignoreline])
+        next if config[:ignoreline] && config[:ignoreline].find { |x| line.match(x) }
+        puts line if config[:debug]
       rescue
         unknown "malformed line from df: #{line}"
       end
+      @line_count += 1
       if capacity.to_i >= config[:crit]
         @crit_fs << "#{mnt} #{capacity}"
       elsif capacity.to_i >= config[:warn]
@@ -73,7 +87,11 @@ class CheckDisk < Sensu::Plugin::Check::CLI
   end
 
   def run
+    if config[:includeline] && config[:ignoreline]
+      unknown 'Do not use -l and -L options concurrently'
+    end
     read_df
+    unknown 'No filesystems found' unless @line_count > 0
     critical usage_summary unless @crit_fs.empty?
     warning usage_summary unless @warn_fs.empty?
     ok "All disk usage under #{config[:warn]}%"
