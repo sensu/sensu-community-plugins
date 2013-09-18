5d672a45ec292d0125ec64137603248479455888
Yet Another Clever Name <admin@yacn.pw>
2013-09-18 15:14:27 -0400

    removes chef stuff from handler

--- handlers/other/ec2_node.rb
+++ handlers/other/ec2_node.rb
@@ -1,11 +1,9 @@
 #!/usr/bin/env ruby
 #
-# This handler removes a Chef node if it has been terminated in EC2 by means
-# that do not remove the information from the Chef Server, such as through the
-# EC2 web console.
+# This handler removes Sensu client if it has been terminated in EC2.
 #
-# NOTE: The implementation for correlating Sensu clients to Chef nodes in EC2
-# may need to be modified to fit your organization. The current implementation
+# NOTE: The implementation for correlating Sensu clients to EC2 instances may
+# need to be modified to fit your organization. The current implementation
 # assumes that Sensu clients' names are the same as their instance IDs in EC2.
 # If this is not the case, you can either sub-class this handler and override
 # `ec2_node_exists?` in your own organization-specific handler, or modify this
@@ -13,22 +11,15 @@
 #
 # Requires the following Rubygems (`gem install $GEM`):
 #   - sensu-plugin
-#   - spice (~> 1.0.6)
 #   - fog
 #
 # Requires the following environment variables to be set:
 #   - AWS_ACCESS_KEY_ID
 #   - AWS_SECRET_ACCESS_KEY
+#   - EC2_REGION
 #
-# Requires a Sensu configuration snippet:
+# Or you can use a Sensu configuration snippet:
 #   {
-#     "chef": {
-#       "server_url": "https://api.opscode.com:443/organizations/vulcan",
-#       "client_name": "spock",
-#       "client_key": "/path/to/spocks/key.pem",
-#       "version": "10.16.4",
-#       "verify_ssl": false
-#     }
 #   }
 #
 # You can use this handler with a filter:
@@ -44,9 +35,9 @@
 #       }
 #     },
 #     "handlers": {
-#       "chef_ec2_node": {
+#       "ec2_node": {
 #         "type": "pipe",
-#         "command": "/etc/sensu/handlers/chef_ec2_node.rb",
+#         "command": "/etc/sensu/handlers/ec2_node.rb",
 #         "filter": "ghost_nodes"
 #       }
 #     }
@@ -59,7 +50,7 @@
 #       "name": "i-424242",
 #       "address": "127.0.0.1",
 #       "keepalive": {
-#         "handler": "chef_ec2_node"
+#         "handler": "ec2_node"
 #       },
 #       "subscriptions": ["all"]
 #     }
@@ -75,34 +66,13 @@
 require 'timeout'
 require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'sensu-handler'
-require 'spice'
 require 'fog'
 
-class ChefEc2Node < Sensu::Handler
-
-  def initialize
-    setup_spice
-  end
+class Ec2Node < Sensu::Handler
 
   def filter; end
 
   def handle
-    if chef_node_exists?
-      delete_chef_node! unless ec2_node_exists?
-    end
-  end
-
-  def chef_node_exists?
-    begin
-      timeout(8) do
-        !!Spice.node(@event['client']['name'])
-      end
-    rescue Spice::Error::NotFound
-      false
-    rescue => error
-      puts "Chef Ec2 Node - Unexpected error: #{error.message}"
-      true
-    end
   end
 
   def ec2_node_exists?
@@ -117,27 +87,6 @@ class ChefEc2Node < Sensu::Handler
       return true if id == @event['client']['name']
     end
     return false # no match found, node doesn't exist
-  end
-
-  def delete_chef_node!
-    cmd = "/nodes/#{@event['client']['name']}"
-    Spice.delete(cmd)
-  end
-
-  def setup_spice
-    Spice.setup do |s|
-      s.server_url   = settings['chef']['server_url']
-      s.client_name  = settings['chef']['client_name']
-      s.client_key   = Spice.read_key_file(settings['chef']['client_key'])
-      s.chef_version = settings['chef']['version'] || '11.0.0'
-      unless settings['chef']['verify_ssl'].nil?
-        s.connection_options = {
-          :ssl => {
-            :verify => settings['chef']['verify_ssl']
-          }
-        }
-      end
-    end
   end
 
 end
