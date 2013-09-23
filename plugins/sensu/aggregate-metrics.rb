9976f0df82f614e11bbe6d8ad2a608242f33b076
Nick Stielau <nick.stielau@gmail.com>
2013-09-23 06:41:58 -0700

    Adding plugin to record aggregate metrics.

---
+++ plugins/sensu/aggregate-metrics.rb
@@ -0,0 +1,143 @@
+#!/usr/bin/env ruby
+#
+# Aggregate Metrics
+#
+# Walks the /aggregates API to return metrics for
+# the aggregated output states of each check.
+# ===
+#
+# Authors
+# ===
+# Sean Porter, @portertech
+# Nick Stielau, @nstielau
+#
+# Copyright 2013 Nick Stielau
+# Copyright 2012 Sonian, Inc.
+#
+# Released under the same terms as Sensu (the MIT license); see
+# LICENSE for details.
+
+require "rubygems"
+require "sensu-plugin/check/cli"
+require "rest-client"
+require "json"
+
+class AggregateMetrics < Sensu::Plugin::Metric::CLI::Graphite
+  option :api,
+    :short => "-a URL",
+    :long => "--api URL",
+    :description => "Sensu API URL",
+    :default => "http://localhost:4567"
+
+  option :user,
+    :short => "-u USER",
+    :long => "--user USER",
+    :description => "Sensu API USER"
+
+  option :password,
+    :short => "-p PASSOWRD",
+    :long => "--password PASSWORD",
+    :description => "Sensu API PASSWORD"
+
+  option :timeout,
+    :short => "-t SECONDS",
+    :long => "--timeout SECONDS",
+    :description => "Sensu API connection timeout in SECONDS",
+    :proc => proc {|a| a.to_i },
+    :default => 30
+
+  option :age,
+    :short => "-A SECONDS",
+    :long => "--age SECONDS",
+    :description => "Minimum aggregate age in SECONDS, time since check request issued",
+    :default => 30,
+    :proc => proc {|a| a.to_i }
+
+  option :summarize,
+    :short => "-s",
+    :long => "--summarize",
+    :boolean => true,
+    :description => "Summarize check result output",
+    :default => false
+
+  option :warning,
+    :short => "-W PERCENT",
+    :long => "--warning PERCENT",
+    :description => "PERCENT non-ok before warning",
+    :proc => proc {|a| a.to_i }
+
+  option :critical,
+    :short => "-C PERCENT",
+    :long => "--critical PERCENT",
+    :description => "PERCENT non-ok before critical",
+    :proc => proc {|a| a.to_i }
+
+  option :scheme,
+    :description => "Metric naming scheme",
+    :long => "--scheme SCHEME",
+    :default => "#{Socket.gethostname}.sensu.aggregates"
+
+  option :verbose,
+    :short => "-v"
+    :description => "Verbose output"
+
+  def api_request(resource)
+    begin
+      request = RestClient::Resource.new(config[:api] + resource, {
+        :timeout => config[:timeout],
+        :user => config[:user],
+        :password => config[:password]
+      })
+      JSON.parse(request.get, :symbolize_names => true)
+    rescue RestClient::ResourceNotFound
+      warning "Resource not found: #{resource}"
+    rescue Errno::ECONNREFUSED
+      warning "Connection refused"
+    rescue RestClient::RequestFailed
+      warning "Request failed"
+    rescue RestClient::RequestTimeout
+      warning "Connection timed out"
+    rescue RestClient::Unauthorized
+      warning "Missing or incorrect Sensu API credentials"
+    rescue JSON::ParserError
+      warning "Sensu API returned invalid JSON"
+    end
+  end
+
+  def get_checks
+    uri = "/aggregates"
+    checks = api_request(uri)
+    puts "Checks: #{checks.inspect}" if config[:verbose]
+    return checks
+  end
+
+  def get_aggregate(check)
+    uri = "/aggregates/#{check}"
+    issued = api_request(uri + "?age=#{config[:age]}")
+    unless issued.empty?
+      issued_sorted = issued.sort
+      time = issued_sorted.pop
+      unless time.nil?
+        uri += "/#{time}"
+        if config[:summarize]
+          uri += "?summarize=output"
+        end
+        time, api_request(uri)
+      else
+        warning "No aggregates older than #{config[:age]} seconds"
+      end
+    else
+      warning "No aggregates for #{check}"
+    end
+  end
+
+  def run
+    get_checks.each do |check|
+      timestamp, aggregate = get_aggregate(check)
+      puts "#{check} aggregates: #{aggregate}" if config[:verbose]
+      aggregate.each do |result, count|
+        output "#{config[:scheme]}.#{check}.#{result}", count, timestamp
+      end
+    end
+  end
+end
