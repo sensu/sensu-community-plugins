c1615bb5e74dbda680f571cfbda8725b89f48805
Jonathon W. Marshall <jonathon@bnotions.com>
2013-10-11 13:18:44 -0400

    remove params hash

--- handlers/notification/mailer.rb
+++ handlers/notification/mailer.rb
@@ -25,6 +25,9 @@ class Mailer < Sensu::Handler
   end
 
   def handle
+    mail_to = settings['mailer']['mail_to']
+    mail_from =  settings['mailer']['mail_from']
+
     smtp_address = settings['mailer']['smtp_address'] || 'localhost'
     smtp_port = settings['mailer']['smtp_port'] || '25'
     smtp_domain = settings['mailer']['smtp_domain'] || 'localhost.localdomain'
@@ -32,17 +35,6 @@ class Mailer < Sensu::Handler
     smtp_username = settings['mailer']['smtp_username'] || nil
     smtp_password = settings['mailer']['smtp_password'] || nil
     smtp_authentication = settings['mailer']['smtp_authentication'] || :plain
-
-    params = {
-      :mail_to   => settings['mailer']['mail_to'],
-      :mail_from => settings['mailer']['mail_from'],
-      :smtp_addr => smtp_address,
-      :smtp_port => smtp_port,
-      :smtp_domain => smtp_domain,
-      :smtp_username => smtp_username,
-      :smtp_password => smtp_password,
-      :smtp_authenticaiton => smtp_authentication
-    }
 
     playbook = "Playbook:  #{@event['check']['playbook']}" if @event['check']['playbook']
     body = <<-BODY.gsub(/^\s+/, '')
@@ -60,18 +52,18 @@ class Mailer < Sensu::Handler
 
     Mail.defaults do
       delivery_options = {
-        :address    => params[:smtp_addr],
-        :port       => params[:smtp_port],
-        :domain     => params[:smtp_domain],
+        :address    => smtp_addr,
+        :port       => smtp_port,
+        :domain     => smtp_domain,
         :openssl_verify_mode => 'none',
         :enable_starttls_auto => true
       }
 
-      unless params[:smtp_username].nil?
+      unless smtp_username.nil?
         auth_options = {
-        :user_name        => params[:smtp_username],
-        :password         => params[:smtp_password],
-        :authentication   => params[:smtp_authentication]
+          :user_name        => smtp_username,
+          :password         => smtp_password,
+          :authentication   => smtp_authentication
         }
         delivery_options.merge! auth_options
       end
@@ -82,13 +74,13 @@ class Mailer < Sensu::Handler
     begin
       timeout 10 do
         Mail.deliver do
-          to      params[:mail_to]
-          from    params[:mail_from]
+          to      mail_to
+          from    mail_from
           subject subject
           body    body
         end
 
-        puts 'mail -- sent alert for ' + short_name + ' to ' + params[:mail_to].to_s
+        puts 'mail -- sent alert for ' + short_name + ' to ' + mail_to.to_s
       end
     rescue Timeout::Error
       puts 'mail -- timed out while attempting to ' + @event['action'] + ' an incident -- ' + short_name
