5a4cb7701ec57604443e7073e991d68cc0676a78
joe miller <joeym@joeym.net>
2013-11-08 14:13:30 -0800

    add option to calculate load average per processor instead

--- plugins/system/load-metrics.rb
+++ plugins/system/load-metrics.rb
@@ -6,6 +6,16 @@
 # This plugin uses uptime to collect load metrics
 # Basically copied from sensu-community-plugins/plugins/system/vmstat-metrics.rb
 #
+# Load per processor
+# ------------------
+#
+# Optionally, with `--load-per-proc`, this plugin will calculate load per
+# processor from the raw load average by dividing by the number of processors.
+#
+# The number of CPUs is determined by reading `/proc/cpuinfo`. This makes the
+# feature Linux specific. Other OSs can be supported by adding OS # detection
+# and a method to determine the number of CPUs.
+#
 # Released under the same terms as Sensu (the MIT license); see LICENSE
 # for details.
 
@@ -20,18 +30,39 @@ class LoadStat < Sensu::Plugin::Metric::CLI::Graphite
     :long => "--scheme SCHEME",
     :default => "#{Socket.gethostname}"
 
+  option :load_per_processor,
+    :description => "Calculate load per processor instead of raw load averages.",
+    :long => "--load-per-proc",
+    :boolean => true,
+    :default => false
+
+  def number_of_cores
+    @cores ||= File.readlines('/proc/cpuinfo').select { |l| l =~ /^processor\s+:/ }.count
+  end
+
   def run
     result = `uptime`.gsub(',', '').split(' ')
     result = result[-3..-1]
 
     timestamp = Time.now.to_i
-    metrics = {
-      :load_avg => {
-         :one => result[0],
-         :five => result[1],
-         :fifteen => result[2]
-       }
-    }
+    if config[:load_per_processor]
+      metrics = {
+        :load_avg => {
+          :one => (result[0].to_f / number_of_cores).round(2),
+          :five => (result[1].to_f / number_of_cores).round(2),
+          :fifteen => (result[2].to_f / number_of_cores).round(2)
+        }
+      }
+    else
+      metrics = {
+        :load_avg => {
+           :one => result[0],
+           :five => result[1],
+           :fifteen => result[2]
+         }
+      }
+    end
+
     metrics.each do |parent, children|
       children.each do |child, value|
         output [config[:scheme], parent, child].join("."), value, timestamp
