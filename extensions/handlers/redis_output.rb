0e616ef8d7f05957692870ff2e1b647e913aa5f9
kcrayon <github.com@crayon.at>
2013-11-08 23:42:49 +0800

    updates for sensu 0.11+

--- extensions/handlers/redis_output.rb
+++ extensions/handlers/redis_output.rb
@@ -1,43 +1,40 @@
-# Send event output to Redis
-# ===
-#
-# Copyright 2013 kcrayon <crayon@crayon.at>
-#
-# Released under the same terms as Sensu (the MIT license); see LICENSE
-# for details.
+module Sensu::Extension
+  class RedisOutput < Handler
+    def definition
+      {
+        type: 'extension',
+        name: 'redis_output',
+      }
+    end
 
-module Sensu
-  module Extension
-    class RedisOutput < Handler
-      def name
-        'redis_output'
-      end
+    def name
+      definition[:name]
+    end
 
-      def description
-        'sends event output to a redis list or channel'
-      end
+    def description
+      'outputs events output to a redis list or channel'
+    end
 
-      def run(event, settings, &block)
-        opts = settings["redis_output"]
+    def run(event)
+      opts = @settings["redis_output"]
 
-        opts["db"]   ||= 0
-        opts["port"] ||= 6379
-        @@redis ||= Redis.connect(:host => opts["host"], :port => opts["port"], :db => opts["db"])
+      opts["db"]   ||= 0
+      opts["port"] ||= 6379
+      @redis ||= Sensu::Redis.connect(:host => opts["host"], :port => opts["port"], :db => opts["db"])
 
-        output = JSON.parse(event)["check"]["output"]
-        output = opts["split"] ? output.split("\n") : Array(output)
+      output = Oj.load(event)[:check][:output]
+      output = opts["split"] ? output.split("\n") : Array(output)
 
-        output.each do |e|
-          case opts["data_type"]
-          when "list"
-            @@redis.lpush(opts["key"], e)
-          when "channel"
-            @@redis.publish(opts["key"], e)
-          end
+      output.each do |e|
+        case opts["data_type"]
+        when "list"
+          @redis.lpush(opts["key"], e)
+        when "channel"
+          @redis.publish(opts["key"], e)
         end
-
-        block.call("sent #{output.count} events", 0)
       end
+
+      yield("sent #{output.count} events", 0)
     end
   end
 end
