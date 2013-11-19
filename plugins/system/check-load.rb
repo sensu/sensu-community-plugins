056b38002d334e312300420771862062280ac589
Max Griffiths <maxgriff@thoughtworks.com>
2013-11-19 21:17:36 +0000

    Don't force ruby 1.9 in check-load

--- plugins/system/check-load.rb
+++ plugins/system/check-load.rb
@@ -10,8 +10,23 @@
 
 # rubocop:disable HandleExceptions
 
-require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'sensu-plugin/check/cli'
+
+if RUBY_VERSION < '1.9.0'
+  require 'rubygems'
+
+  # round(n) doesn't exist in ruby < 1.9
+  class Float
+    alias oldround:round
+      def round(precision = nil)
+          if precision.nil?
+              return self
+          else
+              return ((self * 10**precision).oldround.to_f) / (10**precision)
+          end
+      end
+  end
+end
 
 class LoadAverage
   def initialize(per_core = false)
