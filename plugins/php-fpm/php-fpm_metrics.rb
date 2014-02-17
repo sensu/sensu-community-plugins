631b0a6f7f754ecd78709e04c42686203e0709e5
Yann VERRY <yann@verry.org>
2014-02-17 16:58:53 +0100

    fix scheme usage

--- plugins/php-fpm/php-fpm_metrics.rb
+++ plugins/php-fpm/php-fpm_metrics.rb
@@ -51,11 +51,13 @@ class PHPfpmMetrics < Sensu::Plugin::Metric::CLI::Graphite
     :description => "Metric naming scheme, text to prepend to metric",
     :short => "-s SCHEME",
     :long => "--scheme SCHEME",
-    :default => "http://"
+    :default => ""
 
   def run
 
-    config[:url] = config[:scheme] + config[:hostname].to_s + ':' + config[:port].to_s + '/' + config[:path].to_s + config[:pool].to_s
+    config[:scheme] = config[:scheme] + '.' + config[:hostname] + '.php_fpm'
+
+    config[:url] = 'http://' + config[:hostname].to_s + ':' + config[:port].to_s + '/' + config[:path].to_s + config[:pool].to_s
     config[:fqdn] = Socket.gethostname
     uri = URI.parse(config[:url])
 
@@ -68,27 +70,27 @@ class PHPfpmMetrics < Sensu::Plugin::Metric::CLI::Graphite
     response.body.split(/\r?\n/).each do |line|
       if line.match(/^pool:\s+(\w+)/)
         connections = line.match(/^pool:\s+(\w+)/).to_a
-        output "#{config[:fqdn]}.pool_name", connections[1]
+        output "#{config[:scheme]}.pool_name", connections[1]
       end
       if line.match(/^active processes:\s+(\d+)/)
         requests = line.match(/^active processes:\s+(\d+)/).to_a
-        output "#{config[:fqdn]}.active_processes", requests[1]
+        output "#{config[:scheme]}.active_processes", requests[1]
       end
       if line.match(/^accepted conn:\s+(\d+)/)
         requests = line.match(/^accepted conn:\s+(\d+)/).to_a
-        output "#{config[:fqdn]}.accepted_conn", requests[1]
+        output "#{config[:scheme]}.accepted_conn", requests[1]
       end
       if line.match(/^slow requests:\s+(\d+)/)
         requests = line.match(/^slow requests:\s+(\d+)/).to_a
-        output "#{config[:fqdn]}.slow_requests", requests[1]
+        output "#{config[:scheme]}.slow_requests", requests[1]
       end
       if line.match(/^max active processes:\s+(\d+)/)
         requests = line.match(/^max active processes:\s+(\d+)/).to_a
-        output "#{config[:fqdn]}.max_active_processes", requests[1]
+        output "#{config[:scheme]}.max_active_processes", requests[1]
       end
       if line.match(/^max children reached:\s+(\d+)/)
         requests = line.match(/^max children reached:\s+(\d+)/).to_a
-        output "#{config[:fqdn]}.max_children_reached", requests[1]
+        output "#{config[:scheme]}.max_children_reached", requests[1]
       end
     end
     else
