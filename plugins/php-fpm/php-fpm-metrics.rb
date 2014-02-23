b29f0d8fd24aba7fb282caad34c992d1f227eb08
Ilari Mäkelä <ilari@i28.fi>
2014-02-23 20:12:46 +0200

    Removed non-ascii chars from comments

--- plugins/php-fpm/php-fpm-metrics.rb
+++ plugins/php-fpm/php-fpm-metrics.rb
@@ -5,7 +5,7 @@
 #
 # Requires `crack` gem to parse xml.
 #
-# Copyright 2014 Ilari Mäkelä <ilari at i28.fi>
+# Copyright 2014 Ilari Mäkelä ilari at i28.fi
 #
 # Released under the same terms as Sensu (the MIT license); see LICENSE
 # for details.
