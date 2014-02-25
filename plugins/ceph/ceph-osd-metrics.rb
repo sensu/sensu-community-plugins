9b4b5b6c018f33bac8cfe51edba56152200856c1
Stephan Renatus <s.renatus@x-ion.de>
2014-02-25 16:06:16 +0100

    make ceph-osd-metrics use sensu/io
    ...this also introduces the possiblity of setting a timeout.  Besides
    that, this change adds a warning if the glob fails to match anything and
    a tiny change in the way the osd_number is extracted from the socket
    path.

--- plugins/ceph/ceph-osd-metrics.rb
+++ plugins/ceph/ceph-osd-metrics.rb
@@ -24,6 +24,7 @@
 
 require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'sensu-plugin/metric/cli'
+require 'sensu/io'
 require 'json'
 require 'English'
 
@@ -40,6 +41,13 @@ class CephOsdMetrics < Sensu::Plugin::Metric::CLI::Graphite
          :long => '--pattern',
          :default => '/var/run/ceph/ceph-osd.*.asok'
 
+  option :timeout,
+         :description => 'Timeout (default 10)',
+         :short => '-t SEC',
+         :long => '--timeout',
+         :proc => proc { |t| t.to_i },
+         :default => 10
+
   def output_data(h, leader)
     h.each_pair do |key, val|
       if val.is_a?(Hash)
@@ -54,17 +62,17 @@ class CephOsdMetrics < Sensu::Plugin::Metric::CLI::Graphite
 
   def run
     Dir.glob(config[:pattern]).each do |socket|
-      data = `ceph --admin-daemon #{socket} perf dump`
-      if $CHILD_STATUS.exitstatus == 0
-        # Left side of wildcard
-        strip1 = config[:pattern].match(/^.*\*/).to_s.gsub(/\*/, '')
-        # Right side of wildcard
-        strip2 = config[:pattern].match(/\*.*$/).to_s.gsub(/\*/, '')
-        osd_num = socket.gsub(strip1, '').gsub(strip2, '')
+      output, error = Sensu::IO.popen("ceph --admin-daemon #{socket} perf dump", 'r', config[:timeout])
+      unless error
+        regex = Regexp.new("^"+config[:pattern].gsub('.','\.').gsub(/\*/,'(.*)')+"$")
+        osd_num = regex.match(socket)[1]
+
         JSON.parse(data).each do |k, v|
           k = k.gsub(/\/$/, '').gsub(/\//, '_')
           output_data(v, "#{osd_num}.#{k}")
         end
+      else
+        warning 'no OSD socket found'
       end
     end
     ok
