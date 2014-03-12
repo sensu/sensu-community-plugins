d71226b924f22bd35787666d2a41ff68443bd52e
Yann VERRY <yann@verry.org>
2014-03-12 23:19:48 +0100

    syntax fix

--- plugins/aws/elasticache-metrics.rb
+++ plugins/aws/elasticache-metrics.rb
@@ -57,12 +57,12 @@ class ElastiCacheMetrics < Sensu::Plugin::Metric::CLI::Graphite
   option :aws_access_key,
     :short => '-a AWS_ACCESS_KEY',
     :long => '--aws-access-key AWS_ACCESS_KEY',
-    :description => "AWS Access Key. Either set ENV['AWS_ACCESS_KEY_ID'] or provide it as an option",
+    :description => "AWS Access Key. Either set ENV['AWS_ACCESS_KEY_ID'] or provide it as an option"
 
   option :aws_secret_access_key,
     :short => '-k AWS_SECRET_ACCESS_KEY',
     :long => '--aws-secret-access-key AWS_SECRET_ACCESS_KEY',
-    :description => "AWS Secret Access Key. Either set ENV['AWS_SECRET_ACCESS_KEY'] or provide it as an option",
+    :description => "AWS Secret Access Key. Either set ENV['AWS_SECRET_ACCESS_KEY'] or provide it as an option"
 
   option :aws_region,
     :short => '-r AWS_REGION',
