57f7af51f3214bb67bb06e9f4c61436bf9ff8b3c
Jaime Gago <gagojaime@gmail.com>
2014-04-23 15:47:29 -0700

    Update cpu-metrics.rb
    add a new cpu_count metric with number of CPUs

--- plugins/system/cpu-metrics.rb
+++ plugins/system/cpu-metrics.rb
@@ -15,13 +15,15 @@ class CpuGraphite < Sensu::Plugin::Metric::CLI::Graphite
   def run
     cpu_metrics = ['user', 'nice', 'system', 'idle', 'iowait', 'irq', 'softirq', 'steal', 'guest']
     other_metrics = ['ctxt', 'processes', 'procs_running', 'procs_blocked', 'btime', 'intr']
-
+    cpu_count = 0
+    
     File.open("/proc/stat", "r").each_line do |line|
       info = line.split(/\s+/)
       next if info.empty?
       name = info.shift
 
       if name.match(/cpu([0-9]+|)/)
+        cpu_count = cpu_count + 1       
         name = 'total' if name == 'cpu'
         cpu_metrics.size.times { |i| output "#{config[:scheme]}.#{name}.#{cpu_metrics[i]}", info[i] }
       end
@@ -30,7 +32,13 @@ class CpuGraphite < Sensu::Plugin::Metric::CLI::Graphite
         output "#{config[:scheme]}.#{name}", info.last
       end
     end
-
+    
+    if cpu_count > 0
+      # writes the number of cpus, the minus 1 is because /proc/stat/
+      # first line is a "cpu" which is stats for total cpus
+       output "#{config[:scheme]}.cpu_count", cpu_count - 1
+    end
+    
     ok
   end
 
