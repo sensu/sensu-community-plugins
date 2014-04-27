08bc677ee4fcefcd71a38c2d53b99eb2638688d5
rhoml <roml@rhommell.com>
2014-04-27 18:50:52 +0200

    Rename plugin and use VERIFY_PEER on openssl

--- plugins/http/check-https-cert.rb
+++ plugins/http/check-https-cert.rb
@@ -36,7 +36,7 @@ class CheckHTTP < Sensu::Plugin::Check::CLI
       uri = URI.parse(config[:url])
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
 
       http.start do |h|
         @cert = h.peer_cert
