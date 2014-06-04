fdc70499c312895b4cfe7ab6d4a4ae4d2bbf0c84
Vahram Sukyas <vahram@gmail.com>
2014-06-04 09:26:03 -0700

    Convert devicemapper ID's to logical volume names
    Allow for the ability to optionally convert devicemapper id's to their logical volume names.

--- plugins/system/disk-metrics.rb
+++ plugins/system/disk-metrics.rb
@@ -11,6 +11,13 @@ class DiskGraphite < Sensu::Plugin::Metric::CLI::Graphite
     :short => "-s SCHEME",
     :long => "--scheme SCHEME",
     :default => "#{Socket.gethostname}.disk"
+    
+  option :convert,
+    :description => "Convert devicemapper to logical volume name",
+    :short => "-c",
+    :long => "--convert",
+    :default => false
+
 
   def run
     # http://www.kernel.org/doc/Documentation/iostats.txt
@@ -23,6 +30,11 @@ class DiskGraphite < Sensu::Plugin::Metric::CLI::Graphite
     File.open("/proc/diskstats", "r").each_line do |line|
       stats = line.strip.split(/\s+/)
       _major, _minor, dev = stats.shift(3)
+      if config[:convert]
+        if dev =~ /^dm-.*$/
+          dev = `lsblk -P -o NAME /dev/"#{dev}"| cut -d\\" -f2`.chomp!
+      	end
+      end
       next if stats == ['0'].cycle.take(stats.size)
 
       metrics.size.times { |i| output "#{config[:scheme]}.#{dev}.#{metrics[i]}", stats[i] }
