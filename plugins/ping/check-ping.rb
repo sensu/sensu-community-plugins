6ede518b0cf0ad3756460a9613f3d43ae1d55511
Johan van den Dorpe <jvd@dneg.com>
2014-08-21 17:52:49 +0100

    Timeout value should be an integer

--- plugins/ping/check-ping.rb
+++ plugins/ping/check-ping.rb
@@ -37,7 +37,7 @@ class CheckPING < Sensu::Plugin::Check::CLI
     :default => false
 
   def run
-    pt = Net::Ping::External.new(config[:host], nil, config[:timeout])
+    pt = Net::Ping::External.new(config[:host], nil, config[:timeout].to_i)
     if pt.ping?
       ok "ICMP ping successful for host: #{config[:host]}"
     else
