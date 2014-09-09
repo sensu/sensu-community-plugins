d6167eefdd6e03078fe0e8c5ea0b09b6ddfd7647
Paul Hamby <paul.hamby@bancvue.com>
2014-09-09 14:26:53 -0500

    Setting resp.body.size to 0 if there is no response body

--- plugins/http/check-http.rb
+++ plugins/http/check-http.rb
@@ -212,23 +212,29 @@ class CheckHTTP < Sensu::Plugin::Check::CLI
       warning "Certificate will expire #{warn_cert_expire}"
     end
 
+    if res.body.nil?
+      size = "0"
+    else
+      size = res.body.size
+    end
+
     case res.code
       when /^2/
         if config[:redirectto]
           critical "expected redirect to #{config[:redirectto]} but got #{res.code}" + body
         elsif config[:pattern]
           if res.body =~ /#{config[:pattern]}/
-            ok "#{res.code}, found /#{config[:pattern]}/ in #{res.body.size} bytes" + body
+            ok "#{res.code}, found /#{config[:pattern]}/ in #{size} bytes" + body
           else
-            critical "#{res.code}, did not find /#{config[:pattern]}/ in #{res.body.size} bytes: #{res.body[0...200]}..."
+            critical "#{res.code}, did not find /#{config[:pattern]}/ in #{size} bytes: #{res.body[0...200]}..."
           end
         else
-          ok("#{res.code}, #{res.body.size} bytes" + body) unless config[:response_code]
+          ok("#{res.code}, #{size} bytes" + body) unless config[:response_code]
         end
       when /^3/
         if config[:redirectok] || config[:redirectto]
           if config[:redirectok]
-            ok("#{res.code}, #{res.body.size} bytes" + body) unless config[:response_code]
+            ok("#{res.code}, #{size} bytes" + body) unless config[:response_code]
           elsif config[:redirectto]
             if config[:redirectto] == res['Location']
               ok "#{res.code} found redirect to #{res['Location']}" + body
@@ -247,7 +253,7 @@ class CheckHTTP < Sensu::Plugin::Check::CLI
 
     if config[:response_code]
       if config[:response_code] == res.code
-        ok "#{res.code}, #{res.body.size} bytes" + body
+        ok "#{res.code}, #{size} bytes" + body
       else
         critical res.code + body
       end
