e55327dd9e5978574dff1ed3e99823769b4d59ab
kappa <kappa@kappa-at-clp.local>
2014-10-05 23:38:14 +0900

    add commentout

--- plugins/windows/check-ram-usage-windows.rb
+++ plugins/windows/check-ram-usage-windows.rb
@@ -26,12 +26,15 @@ class CheckRamUsageWindows < Sensu::Plugin::Check::CLI
   def getRamUsage
     tempArr1=Array.new
     tempArr2=Array.new
+    #
     IO.popen("typeperf -sc 1 \"Memory\\Available bytes\" ") { |io| io.each { |line| tempArr1.push(line) } }
     temp = tempArr1[2].split(",")[1]
     ramAvailableInBytes = temp[1, temp.length - 3].to_f
+    #
     IO.popen("wmic OS get TotalVisibleMemorySize /Value") { |io| io.each { |line| tempArr2.push(line) } }
     totalRam = tempArr2[4].split('=')[1].to_f
     totalRamInBytes = totalRam*1000.0
+    #
     ramUsePercent=(totalRamInBytes - ramAvailableInBytes)*100.0/(totalRamInBytes)
     ramUsePercent.round(2)
   end
