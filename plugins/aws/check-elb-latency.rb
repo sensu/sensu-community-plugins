7a7bfb38aa09ac91772e8d243c6b3f06bb5d4921
Matt Jones <mjones@yieldbot.com>
2014-11-15 19:55:07 -0500

    Fixed rubocop issues

--- plugins/aws/check-elb-latency.rb
+++ plugins/aws/check-elb-latency.rb
@@ -1,20 +1,34 @@
-#!/usr/bin/env ruby
+#! /usr/bin/env ruby
 #
 # Check ELB Latency
-# =================
 #
-# Check ELB Latency by CloudWatch API.
 #
-# Examples
-# -----------------
+# DESCRIPTION:
+#   This plugin checks the health of an Amazon Elastic Load Balancer.
 #
+# OUTPUT:
+#   plain-text
+#
+# PLATFORMS:
+#   all
+#
+# DEPENDENCIES:
+#   gem: right-aws
+#   gem: sensu-plugin
+#
+# EXAMPLES:
 #     # Warning if any load balancer's latency is over 1 second, critical if over 3 seconds.
 #     check-elb-latency --warning-over 1 --critical-over 3
 #
 #     # Critical if "app" load balancer's latency is over 5 seconds, maximum of last one hour
 #     check-elb-latency --elb-names app --critical-over 5 --statistics maximum --period 3600
 #
-# Copyright 2014 github.com/y13i
+# NOTES:
+#
+# LICENSE:
+#   Copyright 2014 github.com/y13i
+#   Released under the same terms as Sensu (the MIT license); see LICENSE
+#   for details.
 #
 
 require "sensu-plugin/check/cli"
@@ -22,52 +36,54 @@ require "aws-sdk"
 
 class CheckELBLatency < Sensu::Plugin::Check::CLI
   option :access_key_id,
-    short:       "-k N",
-    long:        "--access-key-id ID",
-    description: "AWS access key ID"
+         short:       "-k N",
+         long:        "--access-key-id ID",
+         description: "AWS access key ID"
 
   option :secret_access_key,
-    short:       "-s N",
-    long:        "--secret-access-key KEY",
-    description: "AWS secret access key"
+         short:       "-s N",
+         long:        "--secret-access-key KEY",
+         description: "AWS secret access key"
 
   option :region,
-    short:       "-r R",
-    long:        "--region REGION",
-    description: "AWS region"
+         short:       "-r R",
+         long:        "--region REGION",
+         description: "AWS region"
 
   option :elb_names,
-    short:       "-l N",
-    long:        "--elb-names NAMES",
-    proc:        proc {|a| a.split(/[,;]\s*/)},
-    description: "Load balancer names to check. Separated by , or ;. If not specified, check all load balancers"
+         short:       "-l N",
+         long:        "--elb-names NAMES",
+         proc:        proc { |a| a.split(/[,;]\s*/) },
+         description: "Load balancer names to check. Separated by , or ;. If not specified, check all load balancers"
 
   option :end_time,
-    short:       "-t T",
-    long:        "--end-time TIME",
-    default:     Time.now,
-    proc:        proc {|a| Time.parse a},
-    description: "CloudWatch metric statistics end time"
+         short:       "-t T",
+         long:        "--end-time TIME",
+         default:     Time.now,
+         proc:        proc { |a| Time.parse a },
+         description: "CloudWatch metric statistics end time"
 
   option :period,
-    short:       "-p N",
-    long:        "--period SECONDS",
-    default:     60,
-    proc:        proc {|a| a.to_i},
-    description: "CloudWatch metric statistics period"
+         short:       "-p N",
+         long:        "--period SECONDS",
+         default:     60,
+         # #YELLOW
+         proc:        proc { |a| a.to_i },
+         description: "CloudWatch metric statistics period"
 
   option :statistics,
-    short:       "-S N",
-    long:        "--statistics NAME",
-    default:     :average,
-    proc:        proc {|a| a.downcase.intern},
-    description: "CloudWatch statistics method"
+         short:       "-S N",
+         long:        "--statistics NAME",
+         default:     :average,
+         proc:        proc { |a| a.downcase.intern },
+         description: "CloudWatch statistics method"
 
   %w(warning critical).each do |severity|
     option :"#{severity}_over",
-      long:        "--#{severity}-over SECONDS",
-      proc:        proc {|a| a.to_f},
-      description: "Trigger a #{severity} if latancy is over specified seconds"
+           long:        "--#{severity}-over SECONDS",
+           # #YELLOW
+           proc:        proc { |a| a.to_f },
+           description: "Trigger a #{severity} if latancy is over specified seconds"
   end
 
   def aws_config
@@ -88,7 +104,7 @@ class CheckELBLatency < Sensu::Plugin::Check::CLI
   def elbs
     return @elbs if @elbs
     @elbs = elb.load_balancers.to_a
-    @elbs.select! {|elb| config[:elb_names].include? elb.name} if config[:elb_names]
+    @elbs.select! { |elb| config[:elb_names].include? elb.name } if config[:elb_names]
     @elbs
   end
 
@@ -106,7 +122,7 @@ class CheckELBLatency < Sensu::Plugin::Check::CLI
   end
 
   def latest_value(metric)
-    metric.statistics(statistics_options.merge unit: "Seconds").datapoints.sort_by {|datapoint| datapoint[:timestamp]}.last[config[:statistics]]
+    metric.statistics(statistics_options.merge unit: "Seconds").datapoints.sort_by { |datapoint| datapoint[:timestamp] }.last[config[:statistics]]
   end
 
   def flag_alert(severity, message)
@@ -140,11 +156,11 @@ class CheckELBLatency < Sensu::Plugin::Check::CLI
                 end
 
     @severities = {
-                    critical: false,
-                    warning:  false,
-                  }
+      critical: false,
+      warning:  false,
+    }
 
-    elbs.each {|elb| check_latency elb}
+    elbs.each { |elb| check_latency elb }
 
     @message += "; (#{config[:statistics].to_s.capitalize} within #{config[:period]} seconds "
     @message += "between #{config[:end_time] - config[:period]} to #{config[:end_time]})"
