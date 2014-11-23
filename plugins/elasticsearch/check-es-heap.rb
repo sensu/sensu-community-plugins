24aa1475b486871446f9f294456cc3870e6f4312
Panagiotis Papadomitsos <pj@ezgr.net>
2014-11-23 14:26:52 -0800

    Merged check-es-heap-pct to check-es-heap

--- plugins/elasticsearch/check-es-heap.rb
+++ plugins/elasticsearch/check-es-heap.rb
@@ -17,6 +17,7 @@
 #   rest-client Ruby gem
 #
 # Copyright 2012 Sonian, Inc <chefs@sonian.net>
+# Copyright 2014 Panagiotis Papadomitsos <pj@ezgr.net>
 #
 # Released under the same terms as Sensu (the MIT license); see LICENSE
 # for details.
@@ -62,6 +63,12 @@ class ESHeap < Sensu::Plugin::Check::CLI
     :proc => proc {|a| a.to_i },
     :default => 0
 
+  option :percentage,
+    :short => '-P',
+    :long => '--percentage',
+    :description => 'Use the WARNING and CRITICAL threshold numbers as percentage indicators of the total heap available',
+    :default => false
+
   def get_es_version
     info = get_es_resource('/')
     info['version']['number']
@@ -80,7 +87,7 @@ class ESHeap < Sensu::Plugin::Check::CLI
     end
   end
 
-  def get_heap_used
+  def get_heap_data(return_max = false)
     if Gem::Version.new(get_es_version) >= Gem::Version.new('1.0.0')
       stats = get_es_resource('_nodes/_local/stats?jvm=true')
       node = stats['nodes'].keys.first
@@ -89,21 +96,38 @@ class ESHeap < Sensu::Plugin::Check::CLI
       node = stats['nodes'].keys.first
     end
     begin
-      stats['nodes'][node]['jvm']['mem']['heap_used_in_bytes']
+      if return_max
+        return stats['nodes'][node]['jvm']['mem']['heap_used_in_bytes'], stats['nodes'][node]['jvm']['mem']['heap_max_in_bytes']
+      else
+        stats['nodes'][node]['jvm']['mem']['heap_used_in_bytes']
+      end
     rescue
       warning 'Failed to obtain heap used in bytes'
     end
   end
 
   def run
-    heap_used = get_heap_used
-    message "Heap used in bytes #{heap_used}"
-    if heap_used >= config[:crit]
-      critical
-    elsif heap_used >= config[:warn]
-      warning
+    if config[:percentage]
+      heap_used, heap_max = get_heap_data(true)
+      heap_used_ratio = ((100 * heap_used) / heap_max).to_i
+      message "Heap used in bytes #{heap_used} (#{heap_used_ratio}% full)"
+      if heap_used_ratio >= config[:crit]
+        critical
+      elsif heap_used_ratio >= config[:warn]
+        warning
+      else
+        ok
+      end
     else
-      ok
+      heap_used = get_heap_data(false)
+      message "Heap used in bytes #{heap_used}"
+      if heap_used >= config[:crit]
+        critical
+      elsif heap_used >= config[:warn]
+        warning
+      else
+        ok
+      end
     end
   end
 
