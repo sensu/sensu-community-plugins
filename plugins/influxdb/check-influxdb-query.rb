9cfe98abdfaabc7d3734a7be5dfab9bfd034f420
Fraser Scott <fraser.scott@gmail.com>
2014-11-25 11:37:39 +0000

    fixing rubocop offenses

--- plugins/influxdb/check-influxdb-query.rb
+++ plugins/influxdb/check-influxdb-query.rb
@@ -115,10 +115,10 @@ class CheckInfluxdbQuery < Sensu::Plugin::Check::CLI
 
   def run
 
-    influxdb = InfluxDB::Client.new config[:database], 
-      :host => config[:host], 
+    influxdb = InfluxDB::Client.new config[:database],
+      :host => config[:host],
       :port => config[:port],
-      :username => config[:username], 
+      :username => config[:username],
       :password => config[:password]
 
     value = influxdb.query config[:query]
@@ -129,7 +129,7 @@ class CheckInfluxdbQuery < Sensu::Plugin::Check::CLI
       query_name = config[:query]
     end
 
-    if config[:noresult] and value.empty?
+    if config[:noresult] && value.empty?
       critical "No result for query '#{query_name}'"
     end
 
@@ -138,9 +138,9 @@ class CheckInfluxdbQuery < Sensu::Plugin::Check::CLI
       value = json_path.on(value).first || 0
 
       calc = Dentaku::Calculator.new
-      if config[:critical] and calc.evaluate(config[:critical], value: value)
+      if config[:critical] && calc.evaluate(config[:critical], value: value)
         critical "Value '#{value}' matched '#{config[:critical]}' for query '#{query_name}'"
-      elsif config[:warning] and calc.evaluate(config[:warning], value: value)
+      elsif config[:warning] && calc.evaluate(config[:warning], value: value)
         warning "Value '#{value}' matched '#{config[:warning]}' for query '#{query_name}'"
       else
         ok "Value '#{value}' ok for query '#{query_name}'"
@@ -152,5 +152,3 @@ class CheckInfluxdbQuery < Sensu::Plugin::Check::CLI
   end
 
 end
-
-
