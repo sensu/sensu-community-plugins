d65c110b8eb66d4a64a91ec5dcf6ffdf46518fc3
jherbst <jherbst@tropo.com>
2014-12-12 15:16:41 -0500

    fixed indentation

--- plugins/aws/check-instance-events.rb
+++ plugins/aws/check-instance-events.rb
@@ -98,8 +98,8 @@ class CheckInstanceEvents < Sensu::Plugin::Check::CLI
         rescue Exception => e
           puts "Issue getting instance details for #{id}.  Exception = #{e}"
         end
-         # Pushes 'name(i-xxx)' if the Name tag was found, else it just pushes the id
-         event_instances_with_names << (name == "" ? id : "#{name}(#{id})")
+        # Pushes 'name(i-xxx)' if the Name tag was found, else it just pushes the id
+        event_instances_with_names << (name == "" ? id : "#{name}(#{id})")
       end
       event_instances = event_instances_with_names
     end
