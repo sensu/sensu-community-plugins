b3ade6160dccd3e8725f0fccc923524f86b8630b
Mathias Bogaert <mathias.bogaert@gmail.com>
2014-12-15 14:38:42 +0000

    Small doc fix.

--- plugins/redis/check-redis-memory.rb
+++ plugins/redis/check-redis-memory.rb
@@ -44,7 +44,7 @@ class RedisChecks < Sensu::Plugin::Check::CLI
   option :crit_mem,
     :short => "-c KB",
     :long => "--critmem KB",
-    :description => "Allocated BB of memory on which we'll issue a CRITICAL",
+    :description => "Allocated KB of Redis memory on which we'll issue a CRITICAL",
     :proc => proc {|p| p.to_i },
     :required => true
 
