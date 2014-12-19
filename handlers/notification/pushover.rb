baaaed1819e1a4b31916f7cae143e625f4f8215d
Jordan <eatsleepgame@gmail.com>
2014-12-19 12:16:14 -0600

    Indentation issues

--- handlers/notification/pushover.rb
+++ handlers/notification/pushover.rb
@@ -34,26 +34,26 @@ class Pushover < Sensu::Handler
 
   def pushover
     incident_key = @event['client']['name'] + '/' + @event['check']['name']
-      params = {
-        title: event_name,
-        user: settings['pushover']['userkey'],
-        token: settings['pushover']['token'],
-        message: @event['check']['output']
-      }
-      begin
-        timeout(3) do
-          apiurl = 'https://api.pushover.net/1/messages.json'
-          url = URI.parse(apiurl)
-          req = Net::HTTP::Post.new(url.path)
-          req.set_form_data(params)
-          res = Net::HTTP.new(url.host, url.port)
-          res.use_ssl = true
-          res.verify_mode = OpenSSL::SSL::VERIFY_PEER
-          res.start { |http| http.request(req) }
-          puts res
-        end
-      rescue Timeout::Error
-        puts 'pushover -- timed out while attempting to ' + @event['action'] + ' incident -- ' + incident_key
+    params = {
+      title: event_name,
+      user: settings['pushover']['userkey'],
+      token: settings['pushover']['token'],
+      message: @event['check']['output']
+    }
+    begin
+      timeout(3) do
+        apiurl = 'https://api.pushover.net/1/messages.json'
+        url = URI.parse(apiurl)
+        req = Net::HTTP::Post.new(url.path)
+        req.set_form_data(params)
+        res = Net::HTTP.new(url.host, url.port)
+        res.use_ssl = true
+        res.verify_mode = OpenSSL::SSL::VERIFY_PEER
+        res.start { |http| http.request(req) }
+        puts res
       end
+    rescue Timeout::Error
+      puts 'pushover -- timed out while attempting to ' + @event['action'] + ' incident -- ' + incident_key
+    end
   end
 end
