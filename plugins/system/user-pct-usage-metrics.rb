0361c4584a7a70cf4e5491a7f8db6c7641e5a89a
John VanDyk <jvandyk.git@sysarchitects.com>
2014-12-21 21:13:05 -0600

    Please rubocop

--- plugins/system/user-pct-usage-metrics.rb
+++ plugins/system/user-pct-usage-metrics.rb
@@ -21,36 +21,34 @@ require 'sensu-plugin/metric/cli'
 require 'socket'
 
 class UserPercent < Sensu::Plugin::Metric::CLI::Graphite
-
   option :scheme,
-    :description => "Metric naming scheme prepended to .username",
-    :long => "--scheme SCHEME",
-    :default => "#{Socket.gethostname}.user_percent"
+         description: 'Metric naming scheme prepended to .username',
+         long: '--scheme SCHEME',
+         default: "#{Socket.gethostname}.user_percent"
 
   option :ignore_inactive,
-    :description => "Boolean. If true, ignore users using 0% CPU",
-    :long => "--ignore_inactive",
-    :default => true
+         description: 'Boolean. If true, ignore users using 0% CPU',
+         long: '--ignore_inactive',
+         default: true
 
   def run
     timestamp = Time.now.to_i
     pslist = `ps -A -o user= -o %cpu=`
 
-    users = Hash.new
+    users = {}
     pslist.lines.each do |line|
-      user,value = line.split
-      h = {user => value.to_f}
-      users = users.merge(h) {|key, oldval, newval| newval + oldval }
+      user, value = line.split
+      h = { user => value.to_f }
+      users = users.merge(h) { |_key, oldval, newval| newval + oldval }
     end
-    
+
     if config[:ignore_inactive]
-      users.delete_if {|key, value| value == 0 }
+      users.delete_if { |_key, value| value == 0 }
     end
 
     users.each do |key, value|
-      output [config[:scheme], key].join("."), value, timestamp
+      output [config[:scheme], key].join('.'), value, timestamp
     end
-    ok    
+    ok
   end
-
 end
