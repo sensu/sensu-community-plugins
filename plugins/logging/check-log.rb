6348b610e2674859dadc17830bb89264b354a9d6
Matt Jones <mjones@yieldbot.com>
2014-12-22 13:37:26 -0500

    Add headers

--- plugins/logging/check-log.rb
+++ plugins/logging/check-log.rb
@@ -30,18 +30,6 @@
 #   for details.
 #
 
-
-#!/usr/bin/env ruby
-#
-# Check Log Plugin
-# ===
-#
-#
-#
-#
-# Released under the same terms as Sensu (the MIT license); see LICENSE
-# for details.
-
 require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'sensu-plugin/check/cli'
 require 'fileutils'
