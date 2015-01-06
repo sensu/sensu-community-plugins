e2286b69eda081b4c59226667245e95f4c3a45e1
Abhishek Jain <Abhishek_Jain@intuit.com>
2015-01-06 13:20:47 +0530

    Added basic description to hipchat script on its usage. Added description of client level specification of mailing list to the mailer script.

--- handlers/notification/hipchat.rb
+++ handlers/notification/hipchat.rb
@@ -1,4 +1,27 @@
 #!/usr/bin/env ruby
+#
+# Sensu Handler: hipchat
+#
+# This handler script is used to send notifications to Hipchat rooms.
+#
+# Input:
+#   @event - Event attributes.
+#      @event['action'] - Property to figure out the event type i.e. whether it is create or resolve.
+#      @event['check'] - Map of attributes from the check config which is calling this handler
+#      @event['client'] - Map of attributes from the client config for the clients from which this event is generated.
+#   option: json_config - By default, assumes the hipchat config parameters are in a file called "hipchat.json" with
+#                         "hipchat" being the top-level key of the json. This command line option allows to specify
+#                         a custom file instead of "hipchat.json" to fetch the hipchat config from.
+#
+# Output:
+#    Green coloured notification on the Hipchat room if a resolve event is seen.
+#    Yellow coloured notification used to notify warning if a create event is seen with a status of 1
+#    Red coloured notification used to notify critical if a create event is seen with a status other than 1
+#
+# Note: The default hipchat config is fetched from the predefined json config file which is "hipchat.json" or any other
+#       file defiend using the "json_config" command line option. The hipchat room could also be configured on a per client basis
+#       by defining the "hipchat_room" attribute in the client config file. This will override the default hipchat room where the
+#       alerts are being routed to for that particular client.
 
 require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'sensu-handler'
