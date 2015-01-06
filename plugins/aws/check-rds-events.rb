9992a3efc4624bd04939a5675401cb0095a996d0
Tim Smith <tim@cozy.co>
2015-01-06 08:46:40 +0100

    Rubocop fixes for the rds-events plugin

--- plugins/aws/check-rds-events.rb
+++ plugins/aws/check-rds-events.rb
@@ -36,28 +36,28 @@ require 'aws-sdk'
 
 class CheckRDSEvents < Sensu::Plugin::Check::CLI
   option :aws_access_key,
-    :short => '-a AWS_ACCESS_KEY',
-    :long => '--aws-access-key AWS_ACCESS_KEY',
-    :description => "AWS Access Key. Either set ENV['AWS_ACCESS_KEY_ID'] or provide it as an option",
-    :default => ENV['AWS_ACCESS_KEY_ID']
+         short: '-a AWS_ACCESS_KEY',
+         long: '--aws-access-key AWS_ACCESS_KEY',
+         description: "AWS Access Key. Either set ENV['AWS_ACCESS_KEY_ID'] or provide it as an option",
+         default: ENV['AWS_ACCESS_KEY_ID']
 
   option :aws_secret_access_key,
-    :short => '-s AWS_SECRET_ACCESS_KEY',
-    :long => '--aws-secret-access-key AWS_SECRET_ACCESS_KEY',
-    :description => "AWS Secret Access Key. Either set ENV['AWS_SECRET_ACCESS_KEY'] or provide it as an option",
-    :default => ENV['AWS_SECRET_ACCESS_KEY']
+         short: '-s AWS_SECRET_ACCESS_KEY',
+         long: '--aws-secret-access-key AWS_SECRET_ACCESS_KEY',
+         description: "AWS Secret Access Key. Either set ENV['AWS_SECRET_ACCESS_KEY'] or provide it as an option",
+         default: ENV['AWS_SECRET_ACCESS_KEY']
 
   option :aws_region,
-    :short => '-r AWS_REGION',
-    :long => '--aws-region REGION',
-    :description => "AWS Region (such as eu-west-1).",
-    :default => 'us-east-1'
+         short: '-r AWS_REGION',
+         long: '--aws-region REGION',
+         description: 'AWS Region (such as eu-west-1).',
+         default: 'us-east-1'
 
-  def run
+  def run # rubocop:disable AbcSize
     rds = AWS::RDS::Client.new(
-      :access_key_id      => config[:aws_access_key],
-      :secret_access_key  => config[:aws_secret_access_key],
-      :region             => config[:aws_region])
+      access_key_id: config[:aws_access_key],
+      secret_access_key: config[:aws_secret_access_key],
+      region: config[:aws_region])
 
     begin
       # fetch all clusters identifiers
@@ -66,13 +66,13 @@ class CheckRDSEvents < Sensu::Plugin::Check::CLI
 
       # fetch the last 2 hours of events for each cluster
       clusters.each do |cluster_name|
-        events_record = rds.describe_events({:start_time => (Time.now - 7200).iso8601, :source_type => 'db-instance', :source_identifier => cluster_name })
+        events_record = rds.describe_events(start_time: (Time.now - 7200).iso8601, source_type: 'db-instance', source_identifier: cluster_name)
         next if events_record[:events].empty?
 
         # if the last event is a start maint event then the cluster is still in maint
         maint_clusters.push(cluster_name) if events_record[:events][-1][:message] =~ /has started/ || events_record[:events][-1][:message] =~ /is being/
       end
-    rescue Exception => e
+    rescue => e
       unknown "An error occurred processing AWS RDS API: #{e.message}"
     end
 
