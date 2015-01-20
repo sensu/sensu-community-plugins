2377e67e5eb6cc489f89eef43a2d74f9f5feec17
Mark Harrison <mark@mivok.net>
2015-01-20 10:54:28 -0500

    Add missing require

--- plugins/ssl/check-ssl-host.rb
+++ plugins/ssl/check-ssl-host.rb
@@ -37,6 +37,7 @@ require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'sensu-plugin/check/cli'
 require 'date'
 require 'openssl'
+require 'socket'
 
 class CheckSSLHost < Sensu::Plugin::Check::CLI
   check_name 'check_ssl_host'
