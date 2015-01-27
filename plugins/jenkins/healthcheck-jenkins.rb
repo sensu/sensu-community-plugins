1c12a55b5dce18e809e30a2c3d2308b488e3d40b
cornelf <cornelf@cakesolutions.net>
2015-01-27 17:51:10 +0000

    add jenkins check, metrics and jenkins extendedhealthcheck

---
+++ plugins/jenkins/healthcheck-jenkins.rb
@@ -0,0 +1,67 @@
+#! /usr/bin/env ruby
+#
+#   check-jenkins
+#
+# DESCRIPTION:
+#   This plugin checks that the Jenkins Metrics healthcheck is healthy throughout
+#
+# OUTPUT:
+#   plain text
+#
+# PLATFORMS:
+#   Linux
+#
+# DEPENDENCIES:
+#   gem: sensu-plugin
+#   gem: rest-client
+#   gem: json
+#
+# USAGE:
+#   #YELLOW
+#
+# NOTES:
+#
+# LICENSE:
+#   Copyright 2015, Cornel Foltea cornel.foltea@gmail.com
+#   Released under the same terms as Sensu (the MIT license); see LICENSE
+#   for details.
+#
+
+require 'rubygems' if RUBY_VERSION < '1.9.0'
+require 'sensu-plugin/check/cli'
+require 'rest-client'
+require 'json'
+
+class JenkinsMasterStatus < Sensu::Plugin::Check::CLI
+  option :server,
+         description: 'Jenkins Host',
+         short: '-s SERVER',
+         long: '--server SERVER',
+         default: 'localhost'
+
+  option :uri,
+         description: 'Jenkins Metrics Healthcheck URI',
+         short: '-u URI',
+         long: '--uri URI',
+         default: '/metrics/currentUser/healthcheck'
+
+  def run
+    health_key = 'healthy'
+    r = RestClient::Resource.new("http://#{config[:server]}:8080#{config[:uri]}", timeout: 5).get
+    if r.code == 200
+      healthchecks = JSON.parse(r)
+      healthchecks.each do |_, healthcheck_hash_value|
+        if healthcheck_hash_value.has_key?(health_key) && healthcheck_hash_value[health_key] != true
+          critical 'Jenkins Health Parameters not OK'
+        end
+      end
+      ok 'Jenkins Health Parameters are OK'
+    else
+      critical 'Jenkins Service is not responding'
+    end
+  rescue Errno::ECONNREFUSED
+    critical 'Jenkins Service is not responding'
+  rescue RestClient::RequestTimeout
+    critical 'Jenkins Service Connection timed out'
+  end
+end
