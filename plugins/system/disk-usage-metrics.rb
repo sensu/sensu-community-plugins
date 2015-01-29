7e97ac39ac098a2187d1db49e4fcab6c8233ab5f
Jaime Gago <gagojaime@gmail.com>
2015-01-28 16:07:50 -0800

    Fix offence (for real)

--- plugins/system/disk-usage-metrics.rb
+++ plugins/system/disk-usage-metrics.rb
@@ -98,7 +98,7 @@ class DiskUsageMetrics < Sensu::Plugin::Metric::CLI::Graphite
     delim = config[:flatten] == true ? '_' : '.'
     if config[:disk_type]
       types = config[:disk_type]
-      type_option = types.map { |type| "-t #{type}"}.join(' ')
+      type_option = types.map { |type| "-t #{type}" }.join(' ')
     else
       type_option = ''
     end
