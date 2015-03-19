769e7ddf227793878fdb3b949b8cead68092b1de
michael j talarczyk <mjt@mijit.com>
2015-03-19 08:52:40 -0700

    Fix RuboCop offences

--- plugins/whois/check-whois.rb
+++ plugins/whois/check-whois.rb
@@ -27,47 +27,45 @@ require 'sensu-plugin/check/cli'
 require 'whois'
 
 class WhoisCheck < Sensu::Plugin::Check::CLI
-
   option :domain,
-    :short => "-d DOMAIN",
-    :long  =>"--domain DOMAIN",
-    :required => true,
-    :description => "Domain to check"
+         short: '-d DOMAIN',
+         long: '--domain DOMAIN',
+         required: true,
+         description: 'Domain to check'
 
-  option :warning, 
-    :short => "-w DAYS",
-    :long  => "--warn DAYS",
-    :default => 30,
-    :description => "Warn if fewer than DAYS away"
+  option :warning,
+         short: '-w DAYS',
+         long: '--warn DAYS',
+         default: 30,
+         description: 'Warn if fewer than DAYS away'
 
-  option :critical, 
-    :short => "-c DAYS",
-    :long  => "--critical DAYS",
-    :default => 7,
-    :description => "Critical if fewer than DAYS away"
+  option :critical,
+         short: '-c DAYS',
+         long: '--critical DAYS',
+         default: 7,
+         description: 'Critical if fewer than DAYS away'
 
   option :help,
-    :short => "-h",
-    :long => "--help",
-    :description => "Show this message",
-    :on => :tail,
-    :boolean => true,
-    :show_options => true,
-    :exit => 0
+         short: '-h',
+         long: '--help',
+         description: 'Show this message',
+         on: :tail,
+         boolean: true,
+         show_options: true,
+         exit: 0
 
   def run
-    whois = Whois.whois(self.config[:domain])
+    whois = Whois.whois(config[:domain])
     expires_on = DateTime.parse(whois.expires_on.to_s)
     num_days = (expires_on - DateTime.now).to_i
-    message "#{ self.config[:domain] } expires on #{ expires_on.strftime('%m-%d-%Y') } (#{ num_days } days away)"
+    message "#{ config[:domain] } expires on #{ expires_on.strftime('%m-%d-%Y') } (#{ num_days } days away)"
     case
-    when num_days <= self.config[:critical].to_i
+    when num_days <= config[:critical].to_i
       critical
-    when num_days <= self.config[:warning].to_i
+    when num_days <= config[:warning].to_i
       warning
     else
       ok
     end
   end
-
 end
