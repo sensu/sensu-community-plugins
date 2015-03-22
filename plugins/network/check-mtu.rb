57159480956be7ab142f9e0ecbd42b0ded7d19d5
Robin Robin <robin81@gmail.com>
2015-03-22 11:14:14 -0400

    add class headers

--- plugins/network/check-mtu.rb
+++ plugins/network/check-mtu.rb
@@ -38,6 +38,9 @@
 require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'sensu-plugin/check/cli'
 
+#
+# Check MTU
+#
 class CheckMTU < Sensu::Plugin::Check::CLI
   option :interface,
          short: '-i INTERFACE',
