9575e130444e012aab10e995d54b38100cd90ab6
luisdalves <luis@zauberlabs.com>
2015-04-16 14:47:40 -0300

    Value for SurgeQueueLength in statistic_type Fixed
    Run:
    ./elb-full-metrics.rb -n ELBName -r Region -s AWS
    
    Error:
    Error: exception: The parameter Statistics.member.1 must be a value in the set [ Sum, Maximum, Minimum, SampleCount, Average ].

--- plugins/aws/elb-full-metrics.rb
+++ plugins/aws/elb-full-metrics.rb
@@ -94,7 +94,7 @@ class ELBMetrics < Sensu::Plugin::Metric::CLI::Graphite
       'HTTPCode_ELB_4XX' => 'Sum',
       'HTTPCode_ELB_5XX' => 'Sum',
       'BackendConnectionErrors' => 'Sum',
-      'SurgeQueueLength' => 'Max',
+      'SurgeQueueLength' => 'Maximum',
       'SpilloverCount' => 'Sum'
     }
 
