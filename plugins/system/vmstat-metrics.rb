d777bd2fa9e8b8630860da8a9fa2faa1c34d4e47
Dani Codina <dani.codina@Tecnics-MacBook-Air.local>
2015-04-22 14:30:22 +0200

    Added steal field on cpu metric.

--- plugins/system/vmstat-metrics.rb
+++ plugins/system/vmstat-metrics.rb
@@ -78,7 +78,8 @@ class VMStat < Sensu::Plugin::Metric::CLI::Graphite
         user: result[12],
         system: result[13],
         idle: result[14],
-        waiting: result[15]
+        waiting: result[15],
+        waiting: result[16]
       }
     }
     metrics.each do |parent, children|
